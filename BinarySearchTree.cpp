7d711a7ca7fd6b3b0a75f0d710d92da9eca48570
galangqosodnal <mgalangyk@gmail.com>
2025-06-26 11:02:06 +0700

    menambahkan fungsi inorder untuk traversal pohon biner dan menangani kasus pohon kosong

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -92,4 +92,19 @@ class BinaryTree
             }
         }
 
+        void inorder(Node *ptr)
+        {
+            if (isEmpty())
+            {
+                cout << "Tree is empty" << endl;
+                return;
+            }
+            if (ptr == nullptr)
+                return;
+
+            inorder(ptr->leftchild);
+            cout << ptr->info << ""; // parent
+            inorder(ptr->rightchild);
+        }
+
         
